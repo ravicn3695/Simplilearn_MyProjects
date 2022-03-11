7c013b886d7b096cf4e84949f6b6d44a7716a6d0
Ravichandra N <raviravicn3@gmail.com>
2022-03-12 01:28:49 +0530

    Bubble sorting

---
+++ Phase_1_Projects/src/AssistedPracticeProject/BubbleSort.java
@@ -0,0 +1,40 @@
+package assistedPracticeProject;
+
+public class BubbleSort {
+
+	public static void main(String[] args){
+
+	     int[] arr= {50,20,15,5,100};
+	     bubbleSort(arr);
+	     for(int i=0;i<arr.length;i++){
+
+	        System.out.println(arr[i]);
+	        }
+	    }
+
+	    public static void bubbleSort(int[] arr){
+	        int len = arr.length;
+	        int temp = 0;
+	        for(int i=0;i<len;i++){ //O(n*n)
+	            for (int j=1;j<(len);j++){
+	                if(arr[j-1]>arr[j]){
+	                temp = arr[j-1];
+	                arr[j-1]= arr[j];
+	                arr[j]= temp;
+
+	                }
+	                //bubble sorting
+	                /*  20 15 5  10 25    i=0
+	                 * 15 5 10 20 25      i=1
+	                 * 5 10 15 20 25      i=2
+	                 * 5 10 15 20 25      i=3/4
+	                 * 	
+	                 */
+
+	            }
+
+	        }        
+
+	    }
+
+}
