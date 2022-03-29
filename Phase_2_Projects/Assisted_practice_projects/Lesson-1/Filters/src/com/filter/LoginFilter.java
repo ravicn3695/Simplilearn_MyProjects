8c7be9539ad19d55278c787d277271dc17ca6576
Ravichandra N <raviravicn3@gmail.com>
2022-03-29 16:59:24 +0530

    Servlet Filter

---
+++ Phase_2_Projects/Assisted_practice_projects/Lesson-1/Filters/src/com/filter/LoginFilter.java
@@ -0,0 +1,59 @@
+package com.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		
+		PrintWriter out = response.getWriter();
+		String userid = request.getParameter("userid");
+		
+		if(userid != null && userid.equals("ravi")) {
+		chain.doFilter(request, response);
+		}
+		else {
+			out.print("invalid userid");
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
