67826a42dc71a36de667b719cc541ec23fb3ed91
Ravichandra N <raviravicn3@gmail.com>
2022-04-08 01:17:48 +0530

    spring

---
+++ Phase_3_Projects/Assisted_practice_projects/Lesson-1/spring/src/main/java/com/simplilearn/spring/repository/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package com.simplilearn.spring.repository;
+
+import org.springframework.stereotype.Repository;
+
+//@Repository
+public class EmployeeDAO {  //repository or DAO class
+	
+	public String getDAO() {
+		return "This is DAO method() called...";
+	}
+
+}
